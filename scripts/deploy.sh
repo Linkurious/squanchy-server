05be0e273563c178bb61a7ef67bbb39c9f22e13f
Ytawo <milan.sylvain@gmail.com>
2016-08-02 10:48:07 +0200

    Adjusted deployment script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,9 @@ n 4.4.4
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4430
 
-rm master.zip
+git clone https://github.com/Linkurious/dev-center.git
 
 mkdir data
-mv dev-center-master dev-center
 cd dev-center
 
 ./scripts/write_config.sh

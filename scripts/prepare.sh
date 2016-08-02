95a7b0fd1837a623db4b65d7b5caefb3021f363d
Ytawo <milan.sylvain@gmail.com>
2016-08-02 15:28:13 +0200

    Parent folder is always shown first.

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,4 +1,4 @@
-aptitude install -y vim git npm htop tree
+aptitude install -y vim git npm htop tree screen
 npm install -g n
 n 4.4.4
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080

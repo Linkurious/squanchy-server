3141fad20402909b8115a08105c21132920776c1
Ytawo <milan.sylvain@gmail.com>
2016-08-02 11:08:18 +0200

    Separated root from user commands.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,25 @@
-aptitude install -y vim git npm htop tree
-npm install -g n
-n 4.4.4
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
-iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4430
-
 git clone https://github.com/Linkurious/dev-center.git
 
 mkdir data
 cd dev-center
 
 npm install
+
+read -p "Enter HTTP username: " username
+read -s -p "Enter HTTP password: " password
+echo
+read -s -p "Confirm password: " confirm
+echo
+
+if [ $password != $confirm ] ; then
+  echo -e "\e[91mPassword mismatch.\e[0m"
+  exit 1
+fi
+
+SALT='sand-castle'
+
+hash=`echo -n ${password}${SALT} | sha256sum | head -c 64`
+
+echo Writing config.json...
+
+echo "{\"user\":\"${username}\",\"password\":\"${hash}\",\"http_port\":8080,\"https_port\":4430,\"root\":\"/home/linkurious/data\"}" > config.json
---
+++ scripts/prepare.sh
@@ -0,0 +1,5 @@
+aptitude install -y vim git npm htop tree
+npm install -g n
+n 4.4.4
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4430

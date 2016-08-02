9020e8fd630e32a3c81cb968b731a0837473d89a
Ytawo <milan.sylvain@gmail.com>
2016-08-02 11:00:56 +0200

    Removed config generation from deployment script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4430
 
 git clone https://github.com/Linkurious/dev-center.git
 
+mkdir data
 cd dev-center
 
 read -p "Enter HTTP username: " username
@@ -14,20 +15,4 @@ echo
 read -s -p "Confirm password: " confirm
 echo
 
-if [ $password != $confirm ] ; then
-  echo -e "\e[91mPassword mismatch.\e[0m"
-  exit 1
-fi
-
-SALT='sand-castle'
-
-hash=`echo -n ${password}${SALT} | sha256sum | head -c 64`
-
-echo Writing config.json...
-
-echo "{\"user\":\"${username}\",\"password\":\"${hash}\",\"http_port\":8080,\"https_port\":4430,\"root\":\"/home/linkurious/data\"}" > config.json
-
 npm install
-
-mkdir data
-npm start

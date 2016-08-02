a77fda0adcbf62d9b294954d2e508d83a20d382d
Ytawo <milan.sylvain@gmail.com>
2016-08-02 10:58:51 +0200

    Inlined config script in deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,24 @@ git clone https://github.com/Linkurious/dev-center.git
 
 cd dev-center
 
-bash scripts/write_config.sh
+read -p "Enter HTTP username: " username
+read -s -p "Enter HTTP password: " password
+echo
+read -s -p "Confirm password: " confirm
+echo
+
+if [ $password != $confirm ] ; then
+  echo -e "\e[91mPassword mismatch.\e[0m"
+  exit 1
+fi
+
+SALT='sand-castle'
+
+hash=`echo -n ${password}${SALT} | sha256sum | head -c 64`
+
+echo Writing config.json...
+
+echo "{\"user\":\"${username}\",\"password\":\"${hash}\",\"http_port\":8080,\"https_port\":4430,\"root\":\"/home/linkurious/data\"}" > config.json
 
 npm install
 

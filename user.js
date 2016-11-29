c7c6c7c87b90daa265a6f950a0971c575b38444d
Ytawo <milan.sylvain@gmail.com>
2016-11-29 15:39:22 +0100

    Password is now prompted.

--- user.js
+++ user.js
@@ -1,4 +1,5 @@
 (function () {
+  const prompt = require('prompt');
   const credentials = require('./src/credentials');
   const C = require('./src/config');
 
@@ -9,28 +10,39 @@
 
   var argv = process.argv.slice(2),
       command = argv[0],
-      subdomain, user, password;
+      user = argv[1], subdomain = argv[2];
 
   if (command === 'add') {
-    if (argv.length < 3) exit(1, `Usage: node user.js add <username> <password> [<sub-domain>]`);
+    if (argv.length < 2) exit(1, `Usage: node user.js add <username> [<sub-domain>]`);
 
-    user = argv[1];
-    password = argv[2];
-    subdomain = argv[3];
+    prompt.start();
+    prompt.get({
+      properties: {
+        password: {
+          description: 'Enter the password',
+          required: true,
+          hidden: true
+        }
+      }
+    }, function (err, result) {
+      if (err) {
+        console.error(err);
+        process.exit(3);
+      }
 
-    if (!subdomain) {
-      C.APPS.forEach(app => credentials.add(app, user, password));
-    } else if (C.APPS.indexOf(subdomain) === -1) {
-      exit(2, `sub-domain "${subdomain}" is not registered`);
-    } else {
-      credentials.add(subdomain, user, password);
-    }
+      var password = result.password;
+
+      if (!subdomain) {
+        C.APPS.forEach(app => credentials.add(app, user, password));
+      } else if (C.APPS.indexOf(subdomain) === -1) {
+        exit(2, `sub-domain "${subdomain}" is not registered`);
+      } else {
+        credentials.add(subdomain, user, password);
+      }
+    });
 
   } else if (command === 'del') {
-    if (argv.length < 2) exit(1, `Usage: node user.js del <username> [<sub-domain>]`);
-
-    user = argv[1];
-    subdomain = argv[2];
+    if (argv.length < 1) exit(1, `Usage: node user.js del <username> [<sub-domain>]`);
 
     if (!subdomain) {
       C.APPS.forEach(app => credentials.remove(app, user));

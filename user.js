d93ae16c5e64a6a606fe66c790c851d46d5065e8
Ytawo <milan.sylvain@gmail.com>
2016-12-01 15:14:33 +0100

    #2 Sub-domain is now mandatory when adding/removing a user.

--- user.js
+++ user.js
@@ -13,7 +13,7 @@
       user = argv[1], subdomain = argv[2];
 
   if (command === 'add') {
-    if (argv.length < 2) exit(1, `Usage: node user.js add <username> [<sub-domain>]`);
+    if (argv.length < 2) exit(1, `Usage: node user.js add <username> <sub-domain>`);
 
     prompt.start();
     prompt.get({
@@ -32,26 +32,36 @@
 
       var password = result.password;
 
-      if (!subdomain) {
+      if (subdomain === '*') {
         C.APPS.forEach(app => credentials.add(app, user, password));
       } else if (C.APPS.indexOf(subdomain) === -1) {
         exit(2, `sub-domain "${subdomain}" is not registered`);
       } else {
         credentials.add(subdomain, user, password);
+        console.log(`User ${user} successfully added to ${getDomainName(subdomain)}!`);
       }
     });
 
   } else if (command === 'del') {
-    if (argv.length < 1) exit(1, `Usage: node user.js del <username> [<sub-domain>]`);
+    if (argv.length < 2) exit(1, `Usage: node user.js del <username> <sub-domain>`);
 
-    if (!subdomain) {
+    if (subdomain === '*') {
       C.APPS.forEach(app => credentials.remove(app, user));
     } else if (C.APPS.indexOf(subdomain) === -1) {
       exit(2, `sub-domain "${subdomain}" is not registered`);
     } else {
       credentials.remove(subdomain, user);
+      console.log(`User ${user} successfully removed from ${getDomainName(subdomain)}!`);
     }
   } else {
-    exit(1, `Usage:\n* node user.js add <username> <password> [<sub-domain>]\n* node user.js del <username> [<sub-domain>]`);
+    exit(1, `Usage: node user.js add|del <username> <sub-domain>`);
+  }
+
+  function getDomainName(name) {
+    if (name === '*') {
+      return 'all sub-domains';
+    } else {
+      return `sub-domain "${name}"`;
+    }
   }
 })();

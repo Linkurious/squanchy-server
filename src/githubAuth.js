d71f77006dded77c106ca8404f2ff61b5f1e637a
Francesco Infante <fra.inf@icloud.com>
2017-01-19 15:17:57 +0100

    ask for authorization code

--- src/githubAuth.js
+++ src/githubAuth.js
@@ -4,6 +4,7 @@
 
 const url = require('url');
 const _ = require('lodash');
+const request = require('request');
 
 class GithubAuth {
   constructor(auth) {
@@ -12,13 +13,34 @@ class GithubAuth {
     this.redirectUrl = auth.redirectUrl;
     this.organizationName = auth.organizationName;
     this.teamId = auth.teamId;
+    this.urlPrefix = auth.urlPrefix;
   }
 
   authMiddleware(req, res, next) {
     if (req.session.user) {
       next();
-    } else if (req.param.code) {
-      console.log(req.param.code);
+    } else if (req.query.code) {
+      if (req.session.state !== req.query.state) {
+        // something fishy
+        res.status(400).send("req.session.state !== req.query.state");
+      } else {
+        request.post({
+          form: {
+            code: req.query.code,
+            client_id: this.clientID,
+            client_secret: this.clientSecret,
+            redirect_uri: this.urlPrefix,
+            grant_type: 'authorization_code'
+          },
+          json: true,
+          uri: 'https://www.googleapis.com/oauth2/v4/token'
+        }, (err, res) => {
+          var a;
+          if (true) {
+            console.log('yes');
+          }
+        });
+      }
     } else {
       let state = Math.random().toString(36);
       req.session['TwoStageAuth'] = true;
